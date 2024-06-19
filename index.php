0097ce668edd92114557c4f014ce185df32313bc
chakri1998 <35056463+chakri1998@users.noreply.github.com>
2024-06-19 12:55:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Welcome message
 echo "Welcome to my chakradd website!";
-
+echo "commented the PHP info for debugging !";
 // PHP info for debugging (remove this line for production)
-phpinfo();
+// phpinfo();
 ?>
